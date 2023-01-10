c47a6796cf90465b6064a233a85989789467e990
Jerrod Varney <jerrodvarney@gmail.com>
2023-01-09 17:15:35 -0800

    Complete creating new todos

--- datastore/index.js
+++ datastore/index.js
@@ -8,12 +8,40 @@ var items = {};
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
-  var id = counter.getNextUniqueId();
-  items[id] = text;
-  callback(null, { id, text });
+
+  const storeNewFile = (id, cb) => {
+    let path = exports.dataDir + '/' + id + '.txt';
+
+    fs.writeFile(path, text, (err) => {
+      if (err) {
+        throw ('error creating new file');
+      } else {
+        items[id] = text;
+        cb(null, { id, text});
+      }
+    });
+  };
+
+  var id = counter.getNextUniqueId( (err, id) => {
+    if (err) {
+      throw ('error creating a new ID');
+    } else {
+      storeNewFile(id, callback);
+    }
+  });
 };
 
 exports.readAll = (callback) => {
+  ///// refactor to return an array of ToDos to client upon a GET to the collection /////
+  // read dataDir and build list of files //
+  // store each id from each file as well //
+  // send ToDo id as both id and text to the client for now //
+
+  /////AFTER ALL OTHER REFACTORS/////
+  // refactor test for readAll to expect correct ToDo text //
+  // read about promises to make all the async work together //
+  // 'Promise.all' //
+
   var data = _.map(items, (text, id) => {
     return { id, text };
   });
@@ -21,6 +49,9 @@ exports.readAll = (callback) => {
 };
 
 exports.readOne = (id, callback) => {
+  ///// refactor to read a single ToDo from dataDir based on its id /////
+  // must read contents of ToDo file and send it back to client //
+
   var text = items[id];
   if (!text) {
     callback(new Error(`No item with id: ${id}`));
@@ -30,6 +61,8 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
+  ///// refactor to rewrite the a stored ToDo based on its id /////
+
   var item = items[id];
   if (!item) {
     callback(new Error(`No item with id: ${id}`));
@@ -40,6 +73,8 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
+  ///// refactor to remove a ToDo from dataDir by its id /////
+
   var item = items[id];
   delete items[id];
   if (!item) {
